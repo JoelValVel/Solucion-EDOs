b3b6fc01196c17b7764b0cb1d20d30e15abd2792
JoelValVel <ernesto.valentinv@gmail.com>
2022-10-13 21:10:11 -0500

    Primer intento ODEsolver grado 2
    Primer intento para el solucionador de ODEs de segundo grado donde una de las entradas de la red es la ecuación a resolver y otras entradas son el mínimo y el máximo donde se evalúa la solución. El código funciona pero solo aplicando una condición inicial también proporcionada en la entrada de la red neuronal (por defecto y(0)=ic), falta implementar una segunda condición inicial (y'(0)=algo).

--- ODEsolver.py
+++ ODEsolver.py
@@ -9,10 +9,13 @@ import numpy as np
 
 class ODEsolver(Sequential):
     """Use y_pred for y variable, x for x and dy for dy/dx"""
-    def __init__(self, funcion, **kwargs):
+    def __init__(self, funcion, ic, minval, maxval, **kwargs):
         super().__init__(**kwargs)
         self.loss_tracker = keras.metrics.Mean(name='loss')
         self.funcion = funcion
+        self.ic = ic
+        self.minval = minval
+        self.maxval = maxval
 
     @property
     def metrics(self):
@@ -20,20 +23,22 @@ class ODEsolver(Sequential):
 
     def train_step(self, data):
         batch_size = tf.shape(data)[0]
-        x = tf.random.uniform((batch_size,1),minval=-2,maxval=2)
+        x = tf.random.uniform((batch_size,1),minval=self.minval,maxval=self.maxval)
 
         with tf.GradientTape() as tape:
             #calcula el valor del loss
             with tf.GradientTape() as tape2:
                 tape2.watch(x)
-                y_pred = self(x,training=True)
-            dy = tape2.gradient(y_pred, x)
+                with tf.GradientTape() as tape3:
+                    tape3.watch(x)
+                    y_pred = self(x,training=True)
+                dy = tape3.gradient(y_pred, x)
+            dy2 = tape2.gradient(dy,x)
             x_o = tf.zeros([batch_size,1])
             y_o = self(x_o, training=True)
             eq = eval(self.funcion)
-            ic = y_o - 1. # Default initial condition: y(0)=1
+            ic = y_o - self.ic # Default initial condition: y(0)=self.ic
             loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0., ic)
-
         # Aplicar gradientes
         grads = tape.gradient(loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
