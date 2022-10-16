3a5770c73f09be55636c49f9e8b59b94ef940958
JoelValVel <ernesto.valentinv@gmail.com>
2022-10-16 18:58:31 -0500

    Solución del primer problema
    En el archivo inciso_a_b.ipynb se presentan las gráficas de las funciones propuestas. Experimente varios problemas al modificar y entrenar la red pues no obtenía un buen resultado. Después de muchas pruebas, lo que me dio mejores resultados fue agregar "condiciones iniciales" en los puntos x=-1,0,1, es decir, decirle a la red cuál es el resultado correcto en esos puntos, también modifiqué el tamaño de "x" y aumenté el número de épocas.

--- ODEsolver.py
+++ ODEsolver.py
@@ -9,10 +9,13 @@ import numpy as np
 
 class ODEsolver(Sequential):
     """Use y_pred for y variable, x for x and dy for dy/dx"""
-    def __init__(self, funcion, **kwargs):
+    def __init__(self, funcion,ic11,ic0,ic1, **kwargs):
         super().__init__(**kwargs)
         self.loss_tracker = keras.metrics.Mean(name='loss')
         self.funcion = funcion
+        self.ic11 = ic11
+        self.ic0 = ic0
+        self.ic1 = ic1
 
     @property
     def metrics(self):
@@ -20,19 +23,19 @@ class ODEsolver(Sequential):
 
     def train_step(self, data):
         batch_size = tf.shape(data)[0]
-        x = tf.random.uniform((batch_size,1),minval=-2,maxval=2)
+        x = tf.random.uniform((batch_size,1),minval=-1,maxval=1)
 
         with tf.GradientTape() as tape:
             #calcula el valor del loss
-            with tf.GradientTape() as tape2:
-                tape2.watch(x)
-                y_pred = self(x,training=True)
-            dy = tape2.gradient(y_pred, x)
+            y_pred = self(x,training=True)
             x_o = tf.zeros([batch_size,1])
             y_o = self(x_o, training=True)
-            eq = eval(self.funcion)
-            ic = y_o - 1. # Default initial condition: y(0)=1
-            loss = keras.losses.mean_squared_error(0., eq) + keras.losses.mean_squared_error(0., ic)
+            ic = y_o - self.ic0
+            y_11 = self(-tf.ones([batch_size,1]),training = True)
+            ic2 = y_11-self.ic11
+            y_1 = self(tf.ones([batch_size,1]),training=True)
+            ic3 = y_1-self.ic1
+            loss = keras.losses.mean_squared_error(eval(self.funcion), y_pred) + keras.losses.mean_squared_error(0., ic) + keras.losses.mean_squared_error(0,ic2) + keras.losses.mean_squared_error(0,ic3)
 
         # Aplicar gradientes
         grads = tape.gradient(loss, self.trainable_variables)
